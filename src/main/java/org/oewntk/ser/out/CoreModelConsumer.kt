51698d9480cce956772c5aef28a30eb7a95cbb19
1313ou <1313ou@gmail.com>
2024-04-19 09:31:25 +0200

    fromyaml to kotlin

--- src/main/java/org/oewntk/ser/out/CoreModelConsumer.kt
+++ src/main/java/org/oewntk/ser/out/CoreModelConsumer.kt
@@ -1,50 +1,28 @@
 /*
  * Copyright (c) 2021-2021. Bernard Bou.
  */
+package org.oewntk.ser.out
 
-package org.oewntk.ser.out;
-
-import org.oewntk.model.CoreModel;
-import org.oewntk.model.Serialize;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.function.Consumer;
+import org.oewntk.model.CoreModel
+import org.oewntk.model.Serialize.serializeCoreModel
+import java.io.File
+import java.io.IOException
+import java.util.function.Consumer
 
 /**
  * Main class that serializes the core model.
  *
+ * @property file output file
  * @author Bernard Bou
  */
-public class CoreModelConsumer implements Consumer<CoreModel>
-{
-	/**
-	 * Output dir
-	 */
-	protected final File file;
+class CoreModelConsumer(private val file: File) : Consumer<CoreModel> {
 
-	/**
-	 * Constructor
-	 *
-	 * @param file output file
-	 */
-	public CoreModelConsumer(final File file)
-	{
-		this.file = file;
-	}
-
-	@Override
-	public void accept(final CoreModel model)
-	{
-		Tracing.psInfo.printf("[CoreModel] %s%n", model.getSource());
-
-		try
-		{
-			Serialize.serializeCoreModel(model, file);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace(Tracing.psErr);
+	override fun accept(model: CoreModel) {
+		Tracing.psInfo.printf("[CoreModel] %s%n", model.source)
+		try {
+			serializeCoreModel(model, file)
+		} catch (e: IOException) {
+			e.printStackTrace(Tracing.psErr)
 		}
 	}
 }
--- src/main/java/org/oewntk/ser/out/ModelConsumer.kt
+++ src/main/java/org/oewntk/ser/out/ModelConsumer.kt
@@ -1,51 +1,28 @@
 /*
  * Copyright (c) 2021-2021. Bernard Bou.
  */
+package org.oewntk.ser.out
 
-package org.oewntk.ser.out;
-
-import org.oewntk.model.Model;
-import org.oewntk.model.Serialize;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.function.Consumer;
+import org.oewntk.model.Model
+import org.oewntk.model.Serialize.serializeCoreModel
+import java.io.File
+import java.io.IOException
+import java.util.function.Consumer
 
 /**
  * Main class that serializes the model
  *
+ * @property file output file
  * @author Bernard Bou
  */
-public class ModelConsumer implements Consumer<Model>
-{
-	/**
-	 * Output file
-	 */
-	private final File file;
+class ModelConsumer(private val file: File) : Consumer<Model> {
 
-	/**
-	 * Constructor
-	 *
-	 * @param outDir output directory
-	 */
-	public ModelConsumer(File outDir)
-	{
-		this.file = outDir;
-	}
-
-	@Override
-	public void accept(final Model model)
-	{
-		Tracing.psInfo.printf("[Model] %s%n", Arrays.toString(model.getSources()));
-
-		try
-		{
-			Serialize.serializeCoreModel(model, file);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace(Tracing.psErr);
+	override fun accept(model: Model) {
+		Tracing.psInfo.printf("[Model] %s%n", model.sources.contentToString())
+		try {
+			serializeCoreModel(model, file)
+		} catch (e: IOException) {
+			e.printStackTrace(Tracing.psErr)
 		}
 	}
 }
